af5dd7e16d84aae12cfce8f2e59972f83ce55f6d
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-11 10:46:14 +0530

    r g model acct/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for 'acct/users', class_name: "Acct::User"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
