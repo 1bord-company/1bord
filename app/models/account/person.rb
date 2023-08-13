83598db2353dac7ecc2e96b37af3efea757b4ff7
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-13 10:54:14 +0530

    Create person and company on sign up

--- app/models/account/person.rb
+++ app/models/account/person.rb
@@ -1,5 +1,14 @@
-class Account::Person < ApplicationRecord
-  belongs_to :company, inverse_of: :people
+module Account
+  class Person < ApplicationRecord
+    belongs_to :company, inverse_of: :people
+    before_validation :create_company, on: :create
 
-  has_one :user, inverse_of: :person
+    has_one :user, inverse_of: :person
+
+    private
+
+    def create_company
+      self.company = Company.create! email: email
+    end
+  end
 end
--- app/models/account/user.rb
+++ app/models/account/user.rb
@@ -1,13 +1,24 @@
-class Account::User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+module Account
+  class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  belongs_to :person, inverse_of: :user
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable,
+           :confirmable, :lockable, :timeoutable, :trackable
 
-  has_many :sync__tokens,
-           class_name: 'Sync::Token',
-           as: :authorizer
+    belongs_to :person, inverse_of: :user
+    before_validation :create_profile, on: :create
+
+    has_many :sync__tokens,
+             class_name: 'Sync::Token',
+             as: :authorizer
+
+
+    private
+
+    def create_profile
+      self.person = Person.create! email: email
+    end
+  end
 end
