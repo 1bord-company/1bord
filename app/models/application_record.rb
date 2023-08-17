7a65d189464be8ce3f32b95f506fc3031340af53
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-17 19:16:15 +0530

    move inside class << self

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,29 +1,31 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  def self.belongs_to(name, scope = nil, **options)
-    return super unless cross_domain? name
-    return super if options[:polymorphic]
+  class << self
+    def belongs_to(name, scope = nil, **options)
+      return super unless cross_domain? name
+      return super if options[:polymorphic]
 
-    options.merge! class_name: cross_domain_class_name(name).singularize
+      options.merge! class_name: cross_domain_class_name(name).singularize
 
-    super
-  end
+      super
+    end
 
-  def self.has_many(name, scope = nil, **options, &extension)
-    return super unless cross_domain? name
-    return super if options[:through]
+    def has_many(name, scope = nil, **options, &extension)
+      return super unless cross_domain? name
+      return super if options[:through]
 
-    options.merge! class_name: cross_domain_class_name(name).singularize
+      options.merge! class_name: cross_domain_class_name(name).singularize
 
-    super
-  end
+      super
+    end
 
-  private
+    private
 
-  def self.cross_domain?(name) = name.to_s.include?('__')
+    def cross_domain?(name) = name.to_s.include?('__')
 
-  def self.cross_domain_class_name(relation_name)
-    relation_name.to_s.split('__').map(&:camelize).join('::')
+    def cross_domain_class_name(relation_name)
+      relation_name.to_s.split('__').map(&:camelize).join('::')
+    end
   end
 end
