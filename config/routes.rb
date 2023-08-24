53ffa5dff3f4f86b7c65cb5a8c0a94fbab26ff00
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-24 23:46:23 +0530

    Add asana link to home page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     slack: 'https://slack.com/apps/A05FDCGGTGS-1bord-basic',
     jira: 'https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=we3TCrAJslUROZHxTu1v2xF4HOlxgnwG&scope=read%3Ajira-user%20offline_access&redirect_uri=https%3A%2F%2F8c36-49-37-200-135.ngrok-free.app%2Fxapp%2Fproviders%Jira%2Fredirects%2Fnew&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent',
     heroku: 'https://id.heroku.com/oauth/authorize?client_id=156a329a-f734-4e74-9513-831bc3395f87&response_type=code&scope=global&state={anti-forgery-token}',
-    google: 'https://accounts.google.com/o/oauth2/auth?client_id=838747206274-2s2d33rkvhociuffev4gkj44iesahkfu.apps.googleusercontent.com&response_type=code&redirect_uri=https://3e07-106-196-18-137.ngrok-free.app/xapp/providers/Google/redirects/new&scope=https://www.googleapis.com/auth/admin.directory.user.readonly&access_type=offline'
+    google: 'https://accounts.google.com/o/oauth2/auth?client_id=838747206274-2s2d33rkvhociuffev4gkj44iesahkfu.apps.googleusercontent.com&response_type=code&redirect_uri=https://3e07-106-196-18-137.ngrok-free.app/xapp/providers/Google/redirects/new&scope=https://www.googleapis.com/auth/admin.directory.user.readonly&access_type=offline',
+    asana: 'https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1205290049714465&redirect_uri=https%3A%2F%2F3e07-106-196-18-137.ngrok-free.app%2Fxapp%2Fproviders%2FAsana%2Fredirects%2Fnew&state=<STATE_PARAM>'
   }.each { |key, url| direct key do url end }
 end
