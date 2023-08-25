2ac922f8984357f201617612d20938d77a76424f
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-25 23:13:30 +0530

    Use ATC::B for Slack

--- app/clients/slack/user_access_token_client.rb
+++ app/clients/slack/user_access_token_client.rb
@@ -1,33 +1,6 @@
-require 'net/http'
-
-class Slack::UserAccessTokenClient
-  def self.create(code:)
-    new(code).create
-  end
-
-  def initialize(code)
-    @code = code
-  end
-
-  def create
-    url = URI.parse('https://slack.com/api/oauth.v2.access')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-
-    app_credentials = Rails.application.credentials.providers.slack.app
-
-    data = {
-      'client_id' => app_credentials.client_id,
-      'client_secret' => app_credentials.client_secret,
-      'code' => @code
-    }
-
-    request = Net::HTTP::Post.new(url.path)
-    request.set_form_data(data)
-
-    response = http.request(request)
-
-    json = JSON.parse response.body
+module Slack
+  class UserAccessTokenClient < AccessTokenClient::Base
+    BASE_URL = 'https://slack.com'.freeze
+    TOKEN_PATH = 'api/oauth.v2.access'.freeze
   end
 end
-
