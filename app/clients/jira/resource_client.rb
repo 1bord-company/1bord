945c06f99bef9fb400b1424373168e71230a13c7
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-26 17:06:47 +0530

    Use RC:B in Jira

---
+++ app/clients/jira/resource_client.rb
@@ -0,0 +1,5 @@
+module Jira
+  class ResourceClient < ResourceClient::Base
+    BASE_URL = 'https://api.atlassian.com'.freeze
+  end
+end
--- app/clients/jira/users_client.rb
+++ app/clients/jira/users_client.rb
@@ -1,25 +1,6 @@
-require 'net/http'
-
-class Jira::UsersClient
-  def self.index(token, resource_id) = new(token, resource_id).index
-  def initialize(token, resource_id)
-    @token = token
-    @resource_id = resource_id
-  end
-
-  def index
-    # Set the API endpoint URL
-    url = URI.parse("https://api.atlassian.com/ex/jira/#{@resource_id}/rest/api/3/users")
-
-    # Create the HTTP request
-    request = Net::HTTP::Get.new(url)
-    request['Authorization'] = "Bearer #{@token}"
-
-    # Make the API request
-    response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
-      http.request(request)
-    end
-
-    JSON.parse(response.body)
+module Jira
+  class UsersClient < ResourceClient
+    def self.index(token, resource_id) = new(token).index(resource_id)
+    def index(resource_id) = get("ex/jira/#{resource_id}/rest/api/3/users")
   end
 end
