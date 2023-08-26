33a4c43cd4ab7cde6c970e85e98ae3b0a9b4988a
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-26 09:31:52 +0530

    Use RC:B in A:WC

--- app/clients/asana/workspaces_client.rb
+++ app/clients/asana/workspaces_client.rb
@@ -1,22 +1,9 @@
-require 'net/http'
-
 module Asana
-  class WorkspacesClient
+  class WorkspacesClient < ResourceClient::Base
+    BASE_URL = 'https://app.asana.com/api/1.0'.freeze
+
     def self.index(token) = new(token).index
 
-    def initialize(token) = @token = token
-
-    def index
-      uri = URI.parse 'https://app.asana.com/api/1.0/workspaces'
-      http = Net::HTTP.new uri.host, uri.port
-      http.use_ssl = true
-
-      request = Net::HTTP::Get.new uri.request_uri
-      request['Authorization'] = "Bearer #{@token}"
-
-      response = http.request request
-
-      JSON.parse response.body
-    end
+    def index = get('workspaces')
   end
 end
