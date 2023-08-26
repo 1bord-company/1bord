3d0b04ad121153c15c1b646545ac1f391b3be7c8
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-08-26 09:20:35 +0530

    Extract RC:B from A::WMC

--- app/clients/asana/workspace_memberships_client.rb
+++ app/clients/asana/workspace_memberships_client.rb
@@ -1,36 +1,20 @@
 require 'net/http'
 
 module Asana
-  class WorkspaceMembershipsClient
+  class WorkspaceMembershipsClient < ResourceClient::Base
+    BASE_URL = 'https://app.asana.com/api/1.0'.freeze
+
     def self.index(token, workspace_id) = new(token).index(workspace_id)
     def self.show(token, workspace_membership_id) = new(token).show(workspace_membership_id)
 
     def initialize(token) = @token = token
 
     def index(workspace_id)
-      uri = URI.parse "https://app.asana.com/api/1.0/workspaces/#{workspace_id}/workspace_memberships"
-      http = Net::HTTP.new uri.host, uri.port
-      http.use_ssl = true
-
-      request = Net::HTTP::Get.new uri.request_uri
-      request['Authorization'] = "Bearer #{@token}"
-
-      response = http.request request
-
-      JSON.parse response.body
+      get "workspaces/#{workspace_id}/workspace_memberships"
     end
 
     def show(workspace_membership_id)
-      uri = URI.parse "https://app.asana.com/api/1.0/workspace_memberships/#{workspace_membership_id}"
-      http = Net::HTTP.new uri.host, uri.port
-      http.use_ssl = true
-
-      request = Net::HTTP::Get.new uri.request_uri
-      request['Authorization'] = "Bearer #{@token}"
-
-      response = http.request request
-
-      JSON.parse response.body
+      get "workspace_memberships/#{workspace_membership_id}"
     end
   end
 end
---
+++ app/clients/resource_client/base.rb
@@ -0,0 +1,17 @@
+module ResourceClient
+  class Base
+    def get(url)
+      uri = URI.parse "#{self.class::BASE_URL}/#{url}"
+
+      http = Net::HTTP.new uri.host, uri.port
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new uri.request_uri
+      request['Authorization'] = "Bearer #{@token}"
+
+      response = http.request request
+
+      JSON.parse response.body
+    end
+  end
+end
