673b882f6fd0e3904dd94b491f8146c300e102a3
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-09-02 04:43:14 +0530

    Perform reuadit checks in the same way

--- test/integration/xapp/redirects_test.rb
+++ test/integration/xapp/redirects_test.rb
@@ -4,26 +4,10 @@ class Xapp::RedirectsTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   def setup
+    Ext::Bot.destroy_all
+
     @account__user = account_users(:one)
     sign_in @account__user
-  end
-
-  def self.test_provider(provider, check, diff, &block)
-    base_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, '/base.yml')['base']['cassettes']
-    provider_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, "/#{provider.underscore}.yml")[provider.underscore]['cassettes']
-
-    test "#{provider}:#{check}==#{diff}" do
-      [diff, 0].each do |diff|
-        assert_difference check, diff do
-          VCR.insert_provider_cassettes provider.underscore,
-            (base_cassettes + provider_cassettes) do
-            get_redirect provider, &block
-
-            assert_redirected_to root_path
-          end
-        end
-      end
-    end
   end
 
   def get_redirect(provider)
@@ -32,6 +16,55 @@ class Xapp::RedirectsTest < ActionDispatch::IntegrationTest
     params.merge! yield(creds) if block_given?
 
     get url_for [:new, :xapp, :provider, :redirect, params]
+  end
+
+  def install(provider, &block)
+    base_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, '/base.yml')['base']['cassettes']
+    provider_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, "/#{provider.underscore}.yml")[provider.underscore]['cassettes']
+
+    VCR.insert_provider_cassettes provider.underscore,
+      (base_cassettes + provider_cassettes) do
+        get_redirect provider, &block
+      end
+  end
+
+  def reaudit(provider)
+    base_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, '/base.yml')['base']['cassettes']
+    provider_cassettes = YAML.load_file(__FILE__.gsub /\.rb$/, "/#{provider.underscore}.yml")[provider.underscore]['cassettes']
+
+    VCR.insert_provider_cassettes provider.underscore,
+      provider_cassettes do
+        post url_for %i[account audit]
+      end
+  end
+
+  def reset_ext
+    Ext::Resource.destroy_all
+    Ext::Persona.destroy_all
+    Ext::Role.destroy_all
+  end
+
+  def self.test_provider(provider, &block)
+    record_checks(provider).each do |check, diff|
+      test "Installation:#{provider}:#{check}==#{diff}##{SecureRandom.hex(6)}" do
+        assert_difference(check, diff) { install provider, &block }
+      end
+
+      test "TestReaudit:#{provider}:#{check}==#{0}##{SecureRandom.hex(6)}" do
+        install provider, &block
+
+        assert_difference(check, 0) { reaudit provider }
+      end
+    end
+
+    entity_checks(provider).each do |check, diff|
+      test "RealReaudit:#{provider}:#{check}==#{0}##{SecureRandom.hex(6)}" do
+        install provider, &block
+        reset_ext
+
+        assert_difference(check, diff) { reaudit provider }
+      end
+    end
   end
 
   def self.record_checks(provider)
@@ -53,15 +86,29 @@ class Xapp::RedirectsTest < ActionDispatch::IntegrationTest
     end.to_h
   end
 
-  record_checks('GitHub').each do |check, diff|
-    test_provider 'GitHub', check, diff do |creds|
-      { code: creds.user.code, installation_id: creds.bot.id }
-    end
+  def self.entity_checks(provider)
+    provider_records = YAML.load_file(__FILE__.gsub /\.rb$/, "/#{provider.underscore}.yml")[provider.underscore]['records']
+    base = YAML.load_file(__FILE__.gsub /\.rb$/, "/base.yml")['base']
+    base_templates = base['templates']
+
+    provider_records.flat_map do |k,vs|
+      base_template = base_templates[k]
+
+      vs.map do |v|
+        query = "#{base_template['model']}"
+        query << ".where(#{(base_template['where'] || {}).merge(v['where'] || {}).map{ |name, value| [name, value].join ': ' }.join(', ')})" if v['where'] || base_template['where']
+        query << ".where.not(#{(base_template['where.not'] || {}).merge(v['where.not'] || {}).map{ |name, value| [name, value].join ': ' }.join(', ')})" if v['where.not'] || base_template['where.not']
+        query << '.count'
+        [query, v['count']]
+      end
+    end.to_h
+  end
+
+  test_provider 'GitHub' do |creds|
+    { code: creds.user.code, installation_id: creds.bot.id }
   end
 
   %w[Slack Jira Heroku Google Asana].each do |provider|
-    record_checks(provider).each do |check, diff|
-      test_provider provider, check, diff
-    end
+    test_provider provider
   end
 end
