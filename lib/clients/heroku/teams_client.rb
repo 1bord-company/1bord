8690ba1c7281392ab52eeb4fe6f10751ded3d4c0
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-09-02 13:43:06 +0530

    Move Heroku teams index to yml

--- lib/clients/heroku/teams_client.rb
+++ lib/clients/heroku/teams_client.rb
@@ -1,8 +1,5 @@
 module Heroku
   class TeamsClient < ResourceClient
     include ::ResourceClient::CompressedResponse
-
-    def self.index(token) = new(token).index
-    def index = get('teams')
   end
 end
