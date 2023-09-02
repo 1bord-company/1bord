3104f124112249c0d57df54b034060153e21da15
Syed Fazil Basheer <fazil.basheer@quester.xyz>
2023-09-02 12:20:42 +0530

    Move Asana::WorkspaceMembersihpsClient to yml

--- lib/clients.rb
+++ lib/clients.rb
@@ -1,38 +1,58 @@
-require './lib/clients/access_token_client/base.rb'
-require './lib/clients/resource_client/base.rb'
+require './lib/clients/access_token_client/base'
+require './lib/clients/resource_client/base'
 
 %w[asana].each do |provider|
-  provider_module = Object.const_set provider.camelize, Module.new
+  provider_module =
+    if Object.const_defined? provider.camelize
+      Object.const_get provider.camelize
+    else
+      Object.const_set provider.camelize, Module.new
+    end
 
   data = YAML.load_file(__FILE__.gsub /\.rb$/, "/#{provider}.yml")[provider]
 
-  provider_module.const_set \
-    'BotAccessTokenClient',
-    (Class.new AccessTokenClient::Base do
-      const_set :BASE_URL, 'https://' + data['base_url']
-      const_set :TOKEN_PATH, data['token_path']
-    end)
-
-  provider_resource_client = provider_module.const_set \
-    'ResourceClient',
-    (Class.new ResourceClient::Base do
-      const_set :BASE_URL, "https://#{data['resources_base_url']}"
-    end)
-
-  data['resources'].each do |name, options|
+  if provider_module.const_defined? "#{provider.camelize}::BotAccessTokenClient"
+    provider_module.const_get "#{provider.camelize}::BotAccessTokenClient"
+  else
     provider_module.const_set \
-      "#{name.camelize}Client",
-      (Class.new provider_resource_client do
-        if options['actions'].include? 'show'
-          def self.show(token, resource_id) = new(token).show(resource_id)
-          def show(resource_id) = get("#{resource_name}/#{resource_id}")
-
-        end
-
-        if options['actions'].include? 'index'
-          def self.index(token) = new(token).index
-          def index = get(resource_name)
-        end
+      'BotAccessTokenClient',
+      (Class.new AccessTokenClient::Base do
+        const_set :BASE_URL, 'https://' + data['base_url']
+        const_set :TOKEN_PATH, data['token_path']
       end)
   end
+
+  provider_resource_client =
+    if provider_module.const_defined? "#{provider.camelize}::ResourceClient"
+      provider_module.const_get "#{provider.camelize}::ResourceClient"
+    else
+      provider_module.const_set \
+      'ResourceClient',
+      (Class.new ResourceClient::Base do
+        const_set :BASE_URL, "https://#{data['resources_base_url']}"
+      end)
+    end
+
+  data['resources'].each do |name, options|
+    if provider_module.const_defined? "#{provider.camelize}::#{name.camelize}Client"
+      provider_module.const_get "#{provider.camelize}::#{name.camelize}Client"
+    else
+      provider_module.const_set \
+        "#{name.camelize}Client",
+        (Class.new provider_resource_client do
+          if options['actions'].include? 'show'
+            def self.show(token, resource_id) = new(token).show(resource_id)
+            def show(resource_id) = get("#{resource_name}/#{resource_id}")
+
+          end
+
+          if options['actions'].include? 'index'
+            def self.index(token) = new(token).index
+            def index = get(resource_name)
+          end
+        end)
+      end
+    end
 end
+
+require './lib/clients/asana/workspace_memberships_client'
--- lib/clients/asana/workspace_memberships_client.rb
+++ lib/clients/asana/workspace_memberships_client.rb
@@ -4,10 +4,5 @@ module Asana
     def index(workspace_id)
       get "workspaces/#{workspace_id}/workspace_memberships"
     end
-
-    def self.show(token, workspace_membership_id) = new(token).show(workspace_membership_id)
-    def show(workspace_membership_id)
-      get "workspace_memberships/#{workspace_membership_id}"
-    end
   end
 end
